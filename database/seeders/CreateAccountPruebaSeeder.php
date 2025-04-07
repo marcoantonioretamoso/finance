385af545185e1dd22a4217f8c06781e66c00c7d1
Marco Retamoso <retamosorodriguezmarcoantonio@gmail.com>
2025-04-07 12:08:42 -0400

    create seeder account prueba

---
+++ database/seeders/CreateAccountPruebaSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\UsuarioAccount;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateAccountPruebaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(!Account::where('name', 'Ahorro Prueba')->exists()){
+            Account::create([
+                'id' => 4,
+                'name' => 'Ahorro Prueba',
+                'description' => 'Cuenta de prueba',
+                'amount' => 100,
+            ]);
+            UsuarioAccount::create([
+                'id' => 4,
+                'user_id' => 1,
+                'account_id' => 4
+            ]);
+        }
+    }
+}
