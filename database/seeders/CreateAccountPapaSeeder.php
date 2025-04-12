7a875b14566fc6fe22c5fdd0d5a7631210d69882
Marco Retamoso <retamosorodriguezmarcoantonio@gmail.com>
2025-04-12 08:11:50 -0400

    create account papa

---
+++ database/seeders/CreateAccountPapaSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Account;
+use App\Models\UsuarioAccount;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CreateAccountPapaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if(!Account::where('name', 'Cuenta papá')->exists()){
+            Account::create([
+                'id' => 9,
+                'name' => 'Cuenta papá',
+                'description' => 'Cuenta de papa',
+                'amount' => 0,
+            ]);
+            UsuarioAccount::create([
+                'id' => 10,
+                'user_id' => 1,
+                'account_id' => 9
+            ]);
+        }
+    }
+}
